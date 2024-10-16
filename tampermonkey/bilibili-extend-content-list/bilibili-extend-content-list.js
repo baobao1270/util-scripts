00bfb582e7a8b6f83d2878f01605780bc1fd28ec
Joseph Chris <joseph@josephcz.xyz>
2024-10-16 01:06:57 -0700

    Update bilibili-extend-content-list.js

--- tampermonkey/bilibili-extend-content-list/bilibili-extend-content-list.js
+++ tampermonkey/bilibili-extend-content-list/bilibili-extend-content-list.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站合集列表增高
 // @description  拉高播放页播放列表 | 兼容 Safari/Firefox/Chrome/Edge
-// @version      1.1.1
+// @version      1.1.0
 // @license      WTFPL
 // @author       Joseph Chris <joseph@josephcz.xyz>
 // @icon         https://www.bilibili.com/favicon.ico
@@ -15,8 +15,6 @@
 // @match        https://www.bilibili.com/video/*
 // @run-at       document-body
 // @grant        none
-// @downloadURL https://update.greasyfork.org/scripts/464494/B%E7%AB%99%E5%90%88%E9%9B%86%E5%88%97%E8%A1%A8%E5%A2%9E%E9%AB%98.user.js
-// @updateURL https://update.greasyfork.org/scripts/464494/B%E7%AB%99%E5%90%88%E9%9B%86%E5%88%97%E8%A1%A8%E5%A2%9E%E9%AB%98.meta.js
 // ==/UserScript==
     
 (function() {
